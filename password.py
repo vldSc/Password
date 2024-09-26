78d92cf5db43ce2c27a4e6cc95562ecf4855e534
Vlada Screlae <vlada.screlea@gmail.com>
2024-09-27 01:15:55 +0300

    Added retry logic in a separate 'ask_to_retry' function

--- password.py
+++ password.py
@@ -16,10 +16,9 @@ def check_positive_number(prompt):
 
 def generate_password(nr_letters, nr_symbols, nr_numbers):
     '''Password generation function'''
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    numbers = '0123456789'
+    symbols = '!#$%&()*+'
 
     # Choosing random characters
     password = [random.choice(letters) for _ in range(nr_letters)] + \
@@ -29,6 +28,16 @@ def generate_password(nr_letters, nr_symbols, nr_numbers):
     random.shuffle(password)  # Mixing characters
     return ''.join(password)   # Returning the final password
 
+def ask_to_retry():
+    '''Function to ask the user if they want to retry'''
+    while True:
+        retry = input(
+            "Do you want to try again? Type 'y' for yes or 'n' for no.  ").lower()
+        if retry in ["y", "n"]:
+            return retry
+        else:
+            print("\nAnswer is not valid. Choose again.")
+
 
 def main():
     '''Main function'''
@@ -37,32 +46,24 @@ def main():
     while True:
         # Getting the number of characters from the user
         nr_letters = check_positive_number(
-            "How many letters would you like in your password?\n")
+            "\nHow many letters would you like in your password? ")
         nr_symbols = check_positive_number(
-            "How many symbols would you like?\n")
+            "How many symbols would you like? ")
         nr_numbers = check_positive_number(
-            "How many numbers would you like?\n")
+            "How many numbers would you like? ")
 
         # Checking if there are enough characters for a valid password
         if nr_letters + nr_symbols + nr_numbers > 0:
             password = generate_password(nr_letters, nr_symbols, nr_numbers)
-            print(f"Here is your password: {password}\n")
+            print(f"\nHere is your password: {password}\n")
         else:
-            print("Upss! We can't gnerate a password without characters! Try again! \n")
+            print("\nUpss! We can't gnerate a password without characters! Try again! \n")
 
         # Asking the user if they want to generate another password
-        while True:
-            retry = input(
-                "Do you want to try again?\nType 'y' for yes or 'n' for no.  ").lower()
-            if retry in ["y", "n"]:
-                break
-            else:
-                print("\nAnswer is not valid. Choose again.")
+        retry = ask_to_retry()
 
-        if retry == "y":
-            continue
-        else:
-            print("See you next time!")
+        if retry == "n":
+            print("\nSee you next time!")
             break
 
 
